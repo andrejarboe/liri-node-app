50a34e41526c7b8df55be329930b83ac68cff8e5
andrejarboe <andrejarboe@gmail.com>
2017-12-14 15:57:00 -0500

    start twitter

--- liri.js
+++ liri.js
@@ -1,1 +1,29 @@
-var aCamileCasedBot
+//require 
+var keys = require("./keys.js");
+var twitterKeys = require('twitter');
+var command = process.argv[2];
+
+
+/* Functions
+*******************************/
+
+
+if(command === 'my-tweet'){
+  console.log('im here');
+  var params = {
+    screen_name: 'nodejs'
+  };
+
+  twitterKeys.get('statuses/user_timeline', params, function(error, tweets, response) {
+    if (!error) {
+      console.log(tweets);
+    }
+  });
+
+}else if(command === 'spotify-this-song'){
+
+}else if(command === 'movie-this'){
+  
+}else if(command === 'do-what-it-says'){
+    
+}

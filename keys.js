f66dbf577e90b841264048648ead48fa820e011f
andrejarboe <andrejarboe@gmail.com>
2017-12-18 11:09:02 -0500

    made movie command

--- keys.js
+++ keys.js
@@ -1,10 +1,10 @@
-console.log('this is loaded');
+// console.log("this is loaded");
 
 var twitterKeys = {
-  consumer_key: 'rn3TUEKviQi1GDbg3AExA8vML',
-  consumer_secret: 'hoR7WZNV6ZPQan4RYdvKPH3oXliuDvnX0RhUkNQTSTZFlOmtuY',
-  access_token_key: '941321256363876357-EB5eVNbAXqAu3HWzLGA9joR2EAT26Eb',
-  access_token_secret: 'v9hLSx69dfno6UU6h4uAKiKlqcAQVAg298X1ol0t1casB',
-}
+  consumer_key: "rn3TUEKviQi1GDbg3AExA8vML",
+  consumer_secret: "hoR7WZNV6ZPQan4RYdvKPH3oXliuDvnX0RhUkNQTSTZFlOmtuY",
+  access_token_key: "941321256363876357-EB5eVNbAXqAu3HWzLGA9joR2EAT26Eb",
+  access_token_secret: "v9hLSx69dfno6UU6h4uAKiKlqcAQVAg298X1ol0t1casB"
+};
 
 module.exports = twitterKeys;

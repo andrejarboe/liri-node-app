4bf97405093cc78ac331f3baa9884931e44c427c
andrejarboe <andrejarboe@gmail.com>
2017-12-18 19:32:04 -0500

    make do-what work for spotify

--- liri.js
+++ liri.js
@@ -2,11 +2,15 @@
 var twitter = require("twitter");
 var request = require("request");
 var Spotify = require("node-spotify-api");
+var fs = require('fs');
 
 
 var keys = require("./keys.js");
 
 var command = process.argv[2];
+
+/*
+****************************************/
 
 if (command === "my-tweets") {
   //twitter
@@ -81,5 +85,45 @@ if (command === "my-tweets") {
     }
   });
 } else if (command === "do-what-it-says") {
+  fs.readFile("random.txt", "utf8", function(err, data) {
+    if (err) {
+      return console.log(err);
+    }
+  
+    // Break the string down by comma separation and store the contents into the output array.
+    var output = data.split(",");
+  
+    // Loop Through the newly created output array
+    for (var i = 0; i < output.length; i++) {
 
+      if(output[0]=== "spotify-this-song"){
+        var songName = output[1];
+        var artist = "";
+      
+        var spotify = new Spotify({
+          id: "c4b43d807d6446e283c8bd1668080d02",
+          secret: "6a11da9292694fdfa37beb1732df6c0f"
+        });
+      
+        if(!songName){
+          songName = "The Sign Ace of Base";
+          artist = "Ace of Base";
+        }
+      
+        spotify.search({ type: "track", query: songName}, function(
+          err,
+          data
+        ) {
+          if (err) {
+            return console.log("Error occurred: " + err);
+          }
+          
+          console.log("Artist(s): " +data.tracks.items[0].album.artists[0].name);
+          console.log("The song's name: " +data.tracks.items[0].name);
+          console.log("Song preview: " +data.tracks.items[0].preview_url);
+          console.log("Album: " +data.tracks.items[0].album.name);    
+        });
+      }
+    }
+  });
 }
